41eee9dd45f18b3a3fd7e678a25d04cb1002fb93
ekaa8415 <ekaa84150@gmail.com>
2026-06-04 09:32:28 +0700

    Add fitur Cashback

--- main.py
+++ main.py
@@ -8,20 +8,31 @@ while member.lower() not in ["ya", "tidak"]:
     print("Input tidak valid. Silakan masukkan 'ya' atau 'tidak'.")
     member = input("Apakah Anda Member? (ya/tidak): ")
 
-totalHarga = jumlah * harga
-diskon = 0
+def hitung_diskon():
+    if member.lower() == "ya":
+        return 0.1 
+    else:
+        return 0  
 
-if member.lower() == "ya":
-    diskon += totalHarga * 0.10
-
-if jumlah > 5:
-    diskon += totalHarga * 0.05
-
-totalBayar = totalHarga - diskon
-
+def hitung_cashback():
+    if jumlah > 5:
+        return 0.05 
+    else:
+        return 0 
+    
+diskon = hitung_diskon()
+cashback = hitung_cashback()
+total_harga = jumlah * harga
+total_diskon = total_harga * diskon
+total_bayar = total_harga - total_diskon
+print("\n--- Struk Pembelian ---")
 print(f"Nama Pembeli: {nama}")
 print(f"Nama Barang: {barang}")
 print(f"Jumlah Barang: {jumlah}")
-print(f"Harga Barang: {harga}")
-print(f"Diskon: {diskon}")
-print(f"Total Harga: {totalBayar}")
+print(f"Harga Barang: Rp{harga}")
+print(f"Total Harga: Rp{total_harga}")
+print(f"Diskon: Rp{total_diskon}")
+print(f"Total Bayar: Rp{total_bayar}")
+if jumlah > 5:
+    print(f"Cashback: Rp{total_harga * cashback}")
+else:    print("Cashback: Rp0")
